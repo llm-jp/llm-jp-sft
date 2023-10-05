40698859d68dafe6bca683ffb327f853fe158321
Hirokazu Kiyomaru <h.kiyomaru@gmail.com>
2023-10-05 12:01:18 +0900

    add wandb

--- train.py
+++ train.py
@@ -11,20 +11,8 @@ disable_caching()
 logger = logging.getLogger(__name__)
 
 
-def formatting_prompts_func(example):
-    output_texts = []
-    for i in range(len(example["instruction"])):
-        text = ""
-        if "input" in example and example["input"][i]:
-            text += f"{example['input'][i]}\n\n"
-        text += f"{example['instruction'][i]} ### Response: {example['output'][i]}"
-        output_texts.append(text)
-    return output_texts
-
-
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, help="Path to the data directory")
     parser.add_argument("--data_files", nargs="+", help="Path to the data files")
     parser.add_argument("--model_name_or_path", type=str, help="Path to pretrained model or model identifier")
     parser.add_argument("--output_dir", type=str, help="Path to output directory")
@@ -38,13 +26,13 @@ def main() -> None:
     tokenizer_name_or_path: str = args.tokenizer_name_or_path or args.model_name_or_path
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name_or_path)
 
-    logger.info(f"Loading data from {args.data_path}")
-    data_files = [os.path.join(args.data_path, data_file) for data_file in args.data_files]
+    logger.info(f"Loading data")
+    data_files = args.data_files
     assert len(data_files) > 0, "No data files found"
     dataset = load_dataset("json", data_files=data_files)
 
     logger.info("Formatting prompts")
-    response_template = "### Response:"
+    response_template = "回答："
     collator = DataCollatorForCompletionOnlyLM(response_template, tokenizer=tokenizer)
 
     logger.info("Setting up trainer")
@@ -52,12 +40,15 @@ def main() -> None:
         output_dir=args.output_dir,
         overwrite_output_dir=True,
         num_train_epochs=1,
-        per_device_train_batch_size=4,
-        gradient_accumulation_steps=2,
+        per_device_train_batch_size=1,
+        gradient_accumulation_steps=8,
         learning_rate=1e-5,
+        warmup_ratio=0.1,
         lr_scheduler_type="cosine",
         fp16=True,
-        ddp_backend="nccl",
+        save_steps=50_000,
+        logging_steps=50,
+        report_to="wandb",
     )
 
     trainer = SFTTrainer(
@@ -65,9 +56,9 @@ def main() -> None:
         args=training_args,
         tokenizer=tokenizer,
         train_dataset=dataset["train"],
-        formatting_func=formatting_prompts_func,
+        dataset_text_field="text",
         data_collator=collator,
-        max_seq_length=256,
+        max_seq_length=512,
     )
 
     logger.info("Training")
