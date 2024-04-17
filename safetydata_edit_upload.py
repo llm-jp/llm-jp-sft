fa628334fc3a226fee2cbe07fae0c8f1d85b4eb9
olachinkei <keisuke.kamata@wandb.com>
2024-04-17 22:10:05 +0900

    update saftydata

--- safetydata_edit_upload.py
+++ safetydata_edit_upload.py
@@ -4,25 +4,49 @@ import json
 data_name = "AnswerCarefullyVersion000_Dev"
 project_name = "llm-safety-finetuning"
 entity_name = "wandb-japan"
+dataset = "train" # train or eval
 
 
-def transform_json(input_file_path, output_file_path):
-    with open(input_file_path, 'r', encoding='utf-8') as file:
-        data = json.load(file)
+if dataset=="train":
+    def transform_json(input_file_path, output_file_path):
+        with open(input_file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            for entry in data:
+                new_text = f"USER:以下は、タスクを説明する指示です。要求を適切に満たす応答を書きなさい。{entry['text']}\nASSISTANT:{entry['output']}"
+                transformed_entry = {"text": new_text}
+                json_string = json.dumps(transformed_entry, ensure_ascii=False)
+                file.write(json_string + '\n')
+
+    input_file_path = '../AnswerCarefullyVersion000_Dev.json'
+    output_file_path = '../AnswerCarefullyVersion000_Dev_transformed.json'
+
+    transform_json(input_file_path, output_file_path)
+
+    with wandb.init(project=project_name, entity=entity_name) as run:
+        artifact = wandb.Artifact("AnswerCarefullyVersion000_Dev", type="data")
+        artifact.add_file(output_file_path)
+        run.log_artifact(artifact)
+elif dataset=="eval":
+    def transform_json(input_file_path, output_file_path):
+        with open(input_file_path, 'r', encoding='utf-8') as file:
+            data = json.load(file)
+
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            for entry in data:
+                new_text = f"USER:以下は、タスクを説明する指示です。要求を適切に満たす応答を書きなさい。{entry['text']}\nASSISTANT:{entry['output']}"
+                transformed_entry = {"text": new_text}
+                json_string = json.dumps(transformed_entry, ensure_ascii=False)
+                file.write(json_string + '\n')
+
+    input_file_path = '../AnswerCarefullyVersion000_Dev.json'
+    output_file_path = '../AnswerCarefullyVersion000_Dev_transformed.json'
+
+    transform_json(input_file_path, output_file_path)
+
+    with wandb.init(project=project_name, entity=entity_name) as run:
+        artifact = wandb.Artifact("AnswerCarefullyVersion000_Dev", type="data")
+        artifact.add_file(output_file_path)
+        run.log_artifact(artifact)
     
-    with open(output_file_path, 'w', encoding='utf-8') as file:
-        for entry in data:
-            new_text = f"USER:以下は、タスクを説明する指示です。要求を適切に満たす応答を書きなさい。{entry['text']}\nASSISTANT:{entry['output']}"
-            transformed_entry = {"text": new_text}
-            json_string = json.dumps(transformed_entry, ensure_ascii=False)
-            file.write(json_string + '\n')
-
-input_file_path = '../AnswerCarefullyVersion000_Dev.json'
-output_file_path = '../AnswerCarefullyVersion000_Dev_transformed.json'
-
-transform_json(input_file_path, output_file_path)
-
-with wandb.init(project=project_name, entity=entity_name) as run:
-    artifact = wandb.Artifact("AnswerCarefullyVersion000_Dev", type="data")
-    artifact.add_file(output_file_path)
-    run.log_artifact(artifact)
